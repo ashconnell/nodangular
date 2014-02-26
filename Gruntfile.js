347f15c86bca7ab43698ae3933ac7e4dada66640
Ash Connell <contact@ashconnell.com>
2014-02-26 17:29:00 +1100

    - split out sass and use import now

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'public_src',
-                    src: '**/*.scss',
+                    src: 'styles/styles.scss',
                     dest: 'public_build',
                     ext: '.css'
                 }]
